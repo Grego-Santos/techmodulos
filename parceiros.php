9b866029ac23a3c761baa418729bc52baf664f56
Italo Rodrigues <italo.rgrego@gmail.com>
2019-09-12 11:52:55 -0300

    change class box

--- parceiros.php
+++ parceiros.php
@@ -24,34 +24,34 @@
                 <a class="box box-style-box" href="https://www.cat.com/pt_BR.html" target="_blank" rel="noopener noreferrer">
                     <img src="image/logo-empresa/cat-13.png" alt="" />
                 </a>
-                <a class="box box-style-box" href="" target="_blank" rel="noopener noreferrer">
+                <a class="box box-style-box" href="https://www.chevrolet.com.br/" target="_blank" rel="noopener noreferrer">
                     <img src="image/logo-empresa/chevrolet-03.png" alt="" />
                 </a>
-                <a class="box box-style-box" href="" target="_blank" rel="noopener noreferrer">
+                <a class="box box-style-box" href="https://www.ford.com.br/" target="_blank" rel="noopener noreferrer">
                     <img src="image/logo-empresa/ford-09.png" alt="" />
                 </a>
-                <a class="box box-style-box" href="" target="_blank" rel="noopener noreferrer">
+                <a class="box box-style-box" href="https://www.hyundai.com.br/" target="_blank" rel="noopener noreferrer">
                     <img src="image/logo-empresa/hyundai-07.png" alt="" />
                 </a>
-                <a class="box box-style-box" href="" target="_blank" rel="noopener noreferrer">
+                <a class="box box-style-box" href="https://www.jcb.com/pt-br" target="_blank" rel="noopener noreferrer">
                     <img src="image/logo-empresa/jcb-14.png" alt="" />
                 </a>
-                <a class="box box-style-box" href="" target="_blank" rel="noopener noreferrer">
+                <a class="box box-style-box" href="https://www.deere.com.br/pt/index.html" target="_blank" rel="noopener noreferrer">
                     <img src="image/logo-empresa/john deere-12.png" alt="" />
                 </a>
-                <a class="box box-style-box" href="" target="_blank" rel="noopener noreferrer">
+                <a class="box box-style-box" href="https://www.mercedes-benz.com.br/" target="_blank" rel="noopener noreferrer">
                     <img src="image/logo-empresa/mercedes-06.png" alt="" />
                 </a>
-                <a class="box box-style-box" href="" target="_blank" rel="noopener noreferrer">
+                <a class="box box-style-box" href="http://www.newholland.com.br/" target="_blank" rel="noopener noreferrer">
                     <img src="image/logo-empresa/new holland-11.png" alt="" />
                 </a>
-                <a class="box box-style-box" href="" target="_blank" rel="noopener noreferrer">
+                <a class="box box-style-box" href="https://www.trimble.com.br/" target="_blank" rel="noopener noreferrer">
                     <img src="image/logo-empresa/trimble-15.png" alt="" />
                 </a>
-                <a class="box box-style-box" href="" target="_blank" rel="noopener noreferrer">
+                <a class="box box-style-box" href="https://www.vw.com.br/pt.html" target="_blank" rel="noopener noreferrer">
                     <img src="image/logo-empresa/volkswagen-08.png" alt="" />
                 </a>
-                <a class="box box-style-box" href="" target="_blank" rel="noopener noreferrer">
+                <a class="box box-style-box" href="https://www.volvogroup.com.br/pt-br" target="_blank" rel="noopener noreferrer">
                     <img src="image/logo-empresa/volvo-05.png" alt="" />
                 </a>
             </section>
