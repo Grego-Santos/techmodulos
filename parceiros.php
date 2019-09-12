3831a4e27a64a2fd7e7e3523eaecd1ce2624c4db
Italo Rodrigues <italo.rgrego@gmail.com>
2019-09-12 11:30:33 -0300

    add logomarcas and create class Box inside Parceiros

--- parceiros.php
+++ parceiros.php
@@ -2,7 +2,7 @@
 <html lang="pt-br">
 
 <head>
-    <?php include_once 'head.php';?>
+    <?php include_once 'head.php'; ?>
     <title>Parceiros</title>
 </head>
 
@@ -14,12 +14,51 @@
         <section id="section-parceiros">
             <h1>Procedência,<br>Eficiência,<br>Garantia.</h1>
             <div>
-                <p>Para que possamos realizar com excelência o nosso serviço, precisamos trabalhar com as melhores marcas do mercado!</p>
+                <p>Para que possamos realizar com excelência o nosso serviço, precisamos trabalhar com as <span class="enfase-verde">melhores marcas</span> do mercado!</p>
             </div>
+
+            <section class="tm-flex-wrap">
+                <a class="box box-style-box" href="https://www.caseih.com/latam/pt-br/home" target="_blank" rel="">
+                    <img src="image/logo-empresa/case ih-10.png" alt="" />
+                </a>
+                <a class="box box-style-box" href="https://www.cat.com/pt_BR.html" target="_blank" rel="noopener noreferrer">
+                    <img src="image/logo-empresa/cat-13.png" alt="" />
+                </a>
+                <a class="box box-style-box" href="" target="_blank" rel="noopener noreferrer">
+                    <img src="image/logo-empresa/fiat-03.png" alt="" />
+                </a>
+                <a class="box box-style-box" href="" target="_blank" rel="noopener noreferrer">
+                    <img src="image/logo-empresa/ford-09.png" alt="" />
+                </a>
+                <a class="box box-style-box" href="" target="_blank" rel="noopener noreferrer">
+                    <img src="image/logo-empresa/hyundai-07.png" alt="" />
+                </a>
+                <a class="box box-style-box" href="" target="_blank" rel="noopener noreferrer">
+                    <img src="image/logo-empresa/jcb-14.png" alt="" />
+                </a>
+                <a class="box box-style-box" href="" target="_blank" rel="noopener noreferrer">
+                    <img src="image/logo-empresa/john deere-12.png" alt="" />
+                </a>
+                <a class="box box-style-box" href="" target="_blank" rel="noopener noreferrer">
+                    <img src="image/logo-empresa/mercedes-06.png" alt="" />
+                </a>
+                <a class="box box-style-box" href="" target="_blank" rel="noopener noreferrer">
+                    <img src="image/logo-empresa/new holland-11.png" alt="" />
+                </a>
+                <a class="box box-style-box" href="" target="_blank" rel="noopener noreferrer">
+                    <img src="image/logo-empresa/trimble-15.png" alt="" />
+                </a>
+                <a class="box box-style-box" href="" target="_blank" rel="noopener noreferrer">
+                    <img src="image/logo-empresa/volkswagen-08.png" alt="" />
+                </a>
+                <a class="box box-style-box" href="" target="_blank" rel="noopener noreferrer">
+                    <img src="image/logo-empresa/volvo-05.png" alt="" />
+                </a>
+            </section>
         </section>
     </main>
     <footer>
-        <?php include_once 'rodape.php';?>
+        <?php include_once 'rodape.php'; ?>
     </footer>
 </body>
 
