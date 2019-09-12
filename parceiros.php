b24234e598d965959d7d81dc54dcd339cf4971b9
Italo Rodrigues <italo.rgrego@gmail.com>
2019-09-12 15:53:37 -0300

    change parceiros from my notebook

--- parceiros.php
+++ parceiros.php
@@ -13,53 +13,58 @@
     <main>
         <section id="section-parceiros">
             <img src="image/background-business.jpg" alt="">
-                <h1>
-                    <span>Procedência</span>,<br>
-                    <span>Eficiência</span>,<br>
-                    <span>Garantia.</span>
-                </h1>
-            <div>
-                <p>Para que possamos realizar com excelência o nosso serviço, precisamos trabalhar com as <span class="enfase-verde">melhores marcas</span> do mercado!</p>
-            </div>
+            <section id="section-parceiros-title">
+                <p>✔ PROCEDÊNCIA</p>
+                <p>✔ EFICIÊNCIA</p>
+                <p>✔ GARANTIA</p>
+            </section>
+            <section class="content-center">
+                <section class="box-style-shadow">
+                    <p>Para que possamos realizar com excelência o nosso serviço, precisamos trabalhar com as <span class="enfase-verde">melhores marcas</span> do mercado!</p>
+                </section>
+            </section>
         </section>
 
-        <section class="tm-flex-wrap">
-            <a class="box box-style-box" href="https://www.caseih.com/latam/pt-br/home" target="_blank" rel="">
-                <img src="image/logo-empresa/case ih-10.png" alt="" />
-            </a>
-            <a class="box box-style-box" href="https://www.cat.com/pt_BR.html" target="_blank" rel="noopener noreferrer">
-                <img src="image/logo-empresa/cat-13.png" alt="" />
-            </a>
-            <a class="box box-style-box" href="https://www.chevrolet.com.br/" target="_blank" rel="noopener noreferrer">
-                <img src="image/logo-empresa/chevrolet-03.png" alt="" />
-            </a>
-            <a class="box box-style-box" href="https://www.ford.com.br/" target="_blank" rel="noopener noreferrer">
-                <img src="image/logo-empresa/ford-09.png" alt="" />
-            </a>
-            <a class="box box-style-box" href="https://www.hyundai.com.br/" target="_blank" rel="noopener noreferrer">
-                <img src="image/logo-empresa/hyundai-07.png" alt="" />
-            </a>
-            <a class="box box-style-box" href="https://www.jcb.com/pt-br" target="_blank" rel="noopener noreferrer">
-                <img src="image/logo-empresa/jcb-14.png" alt="" />
-            </a>
-            <a class="box box-style-box" href="https://www.deere.com.br/pt/index.html" target="_blank" rel="noopener noreferrer">
-                <img src="image/logo-empresa/john deere-12.png" alt="" />
-            </a>
-            <a class="box box-style-box" href="https://www.mercedes-benz.com.br/" target="_blank" rel="noopener noreferrer">
-                <img src="image/logo-empresa/mercedes-06.png" alt="" />
-            </a>
-            <a class="box box-style-box" href="http://www.newholland.com.br/" target="_blank" rel="noopener noreferrer">
-                <img src="image/logo-empresa/new holland-11.png" alt="" />
-            </a>
-            <a class="box box-style-box" href="https://www.trimble.com.br/" target="_blank" rel="noopener noreferrer">
-                <img src="image/logo-empresa/trimble-15.png" alt="" />
-            </a>
-            <a class="box box-style-box" href="https://www.vw.com.br/pt.html" target="_blank" rel="noopener noreferrer">
-                <img src="image/logo-empresa/volkswagen-08.png" alt="" />
-            </a>
-            <a class="box box-style-box" href="https://www.volvogroup.com.br/pt-br" target="_blank" rel="noopener noreferrer">
-                <img src="image/logo-empresa/volvo-05.png" alt="" />
-            </a>
+        <section id="section-logomarcas">
+            <h1>Para isso, contamos com os seguintes parceiros:</h1>
+            <section class="tm-flex-wrap">
+                <a class="box box-style-box" href="https://www.caseih.com/latam/pt-br/home" target="_blank" rel="">
+                    <img src="image/logo-empresa/case ih-10.png" alt="" />
+                </a>
+                <a class="box box-style-box" href="https://www.cat.com/pt_BR.html" target="_blank" rel="noopener noreferrer">
+                    <img src="image/logo-empresa/cat-13.png" alt="" />
+                </a>
+                <a class="box box-style-box" href="https://www.chevrolet.com.br/" target="_blank" rel="noopener noreferrer">
+                    <img src="image/logo-empresa/chevrolet-03.png" alt="" />
+                </a>
+                <a class="box box-style-box" href="https://www.ford.com.br/" target="_blank" rel="noopener noreferrer">
+                    <img src="image/logo-empresa/ford-09.png" alt="" />
+                </a>
+                <a class="box box-style-box" href="https://www.hyundai.com.br/" target="_blank" rel="noopener noreferrer">
+                    <img src="image/logo-empresa/hyundai-07.png" alt="" />
+                </a>
+                <a class="box box-style-box" href="https://www.jcb.com/pt-br" target="_blank" rel="noopener noreferrer">
+                    <img src="image/logo-empresa/jcb-14.png" alt="" />
+                </a>
+                <a class="box box-style-box" href="https://www.deere.com.br/pt/index.html" target="_blank" rel="noopener noreferrer">
+                    <img src="image/logo-empresa/john deere-12.png" alt="" />
+                </a>
+                <a class="box box-style-box" href="https://www.mercedes-benz.com.br/" target="_blank" rel="noopener noreferrer">
+                    <img src="image/logo-empresa/mercedes-06.png" alt="" />
+                </a>
+                <a class="box box-style-box" href="http://www.newholland.com.br/" target="_blank" rel="noopener noreferrer">
+                    <img src="image/logo-empresa/new holland-11.png" alt="" />
+                </a>
+                <a class="box box-style-box" href="https://www.trimble.com.br/" target="_blank" rel="noopener noreferrer">
+                    <img src="image/logo-empresa/trimble-15.png" alt="" />
+                </a>
+                <a class="box box-style-box" href="https://www.vw.com.br/pt.html" target="_blank" rel="noopener noreferrer">
+                    <img src="image/logo-empresa/volkswagen-08.png" alt="" />
+                </a>
+                <a class="box box-style-box" href="https://www.volvogroup.com.br/pt-br" target="_blank" rel="noopener noreferrer">
+                    <img src="image/logo-empresa/volvo-05.png" alt="" />
+                </a>
+            </section>
         </section>
     </main>
     <footer>
