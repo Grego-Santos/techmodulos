9f5783bbd3c75e3f8a59db735d86af79c008d8fb
Italo Rodrigues <italo.rgrego@gmail.com>
2019-09-13 15:28:17 -0300

    equipe has finished!

--- equipe.php
+++ equipe.php
@@ -2,7 +2,7 @@
 <html lang="pt-br">
 
 <head>
-    <?php include_once 'head.php';?>
+    <?php include_once 'head.php'; ?>
     <title>Equipe</title>
 </head>
 
@@ -10,11 +10,32 @@
     <header>
         <?php include_once 'menu.php'; ?>
     </header>
-    <main>
-        <h1>Página em construção</h1>
+    <main class="content-column-center mg-50">
+        <img src="image/" alt="">
+        <h1 class="title">Nossa equipe</h1>
+
+        <section class="section-funcionario content-liquid-center">
+            <section class="content-column-center">
+                <img src="image/leandro.jpg" alt="">
+                <p class="negrito">Leandro Reino Lopes</p>
+                <p>Gerente</p>
+                <span class="bar"></span>
+            </section>
+            <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos.</p>
+        </section>
+
+        <section class="section-funcionario content-liquid-center-reverse">
+            <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos.</p>
+            <section class="content-column-center">
+                <img src="image/foto.png" alt="">
+                <p class="negrito">Nome</p>
+                <p>Cargo</p>
+                <span class="bar"></span>
+            </section>
+        </section>
     </main>
     <footer>
-        <?php include_once 'rodape.php';?>
+        <?php include_once 'rodape.php'; ?>
     </footer>
 </body>
 
