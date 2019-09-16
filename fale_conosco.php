53fa0b0cf5dbe312a9b90f6d206a2d657fa355d5
Italo Rodrigues <italo.rgrego@gmail.com>
2019-09-16 10:53:41 -0300

    change endereço

--- fale_conosco.php
+++ fale_conosco.php
@@ -11,9 +11,10 @@
         <?php include_once 'menu.php'; ?>
     </header>
     <main>
-        <section class="content-liquid-center mg-50">
-            <h1>Página em construção</h1>
-
+        <section id="section-maps" class="content-column-center mg-50">
+            <h1 class="title">Endereço</h1>
+            <p>Avenida Tupãssi Nº 1295 - Jardim Paraná</p>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4992.007166374541!2d-53.51799756384406!3d-24.40510326786032!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94f3a84ce8f12ee1%3A0x654f12d5453865d7!2sAssis%20Chateaubriand%2C%20PR%2C%2085935-000!5e0!3m2!1spt-BR!2sbr!4v1568483460561!5m2!1spt-BR!2sbr" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
         </section>
     </main>
     <footer>
