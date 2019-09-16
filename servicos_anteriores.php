1b2e7c4a32d48b3164c289382dad56544371cc0e
Italo Rodrigues <italo.rgrego@gmail.com>
2019-09-16 15:51:47 -0300

    change servicos_ateriores (sory for grean commit kkkk)

--- servicos_anteriores.php
+++ servicos_anteriores.php
@@ -2,7 +2,7 @@
 <html lang="pt-br">
 
 <head>
-    <?php include_once 'head.php';?>
+    <?php include_once 'head.php'; ?>
     <title>Serviços anteriores</title>
 </head>
 
@@ -11,10 +11,36 @@
         <?php include_once 'menu.php'; ?>
     </header>
     <main>
-        <h1>Página em construção</h1>
+        <section id="section-servicos-construction" class="content-column-center pd-50">
+            <section class="content-liquid-center mg-50">
+                <h1>Um pouco do que já ajudamos a construir...</h1>
+            </section>
+            <h2 class="subtitle-servicos-construction">Seja no campo ou na cidade, em pequenas ou grandes construções, a <span class="span-tech-modulos">Tech Módulos</span> está sempre pronta para te ajudar!</h2>
+
+
+            <!-- CADA ITEM DO SERVIÇO -->
+            <section class="card-construction content-liquid-center">
+                <img src="image/trator.png" alt="">
+                <section>
+                    <h1>Conserto da placa controladora de injeção</h1>
+                    <p>A 'máquina' do 'Sr. Zé' 'parou de funcionar em plena colheita do soja...'</p>
+                </section>
+            </section>
+            <!-- // CADA ITEM DO SERVIÇO -->
+
+            <!-- CADA ITEM DO SERVIÇO -->
+            <section class="card-construction content-liquid-center-reverse evenly-fluid ">
+                <section>
+                    <h1>Título</h1>
+                    <p>Textinho</p>
+                </section>
+                <img src="image/trator.png" alt="">
+            </section>
+            <!-- // CADA ITEM DO SERVIÇO -->
+        </section>
     </main>
     <footer>
-        <?php include_once 'rodape.php';?>
+        <?php include_once 'rodape.php'; ?>
     </footer>
 </body>
 
